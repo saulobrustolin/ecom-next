c7467ab181390b2da639c54112f9f23cbda763d8
saulo <saulobrustolin@icloud.com>
2025-03-14 07:16:54 -0300

    product release

--- modules/product/images-product.tsx
+++ modules/product/images-product.tsx
@@ -1,8 +1,14 @@
+'use client'
+
 import { ImagesProductProps } from '@/config/definitions';
 
 import Image from 'next/image';
 
+import { useState } from 'react';
+
 export default function ImagesProduct({ images }: ImagesProductProps) {
+    const [pathSelect, setPathSelect] = useState(images[0].path);
+
   return (
     <div
       className='flex gap-2'
@@ -13,13 +19,12 @@ export default function ImagesProduct({ images }: ImagesProductProps) {
         {images.length > 0 ? (
           images.map((image, index) => (
             (images.length == 5) ? (
-              <Image key={index} src={image.path} alt={`Imagem ${index}`} width={100} height={100} className='border border-stone-500/50 rounded-md h-[calc(100%/4)]'/>
+              <Image key={index} src={image.path} alt={`Imagem ${index}`} width={100} height={100} className={'border border-stone-500/50 rounded-md h-[calc(100%/4)]'}/>
             ) : images.length > 5 && index < 3 ? (
               <Image key={index} src={image.path} alt={`Imagem ${index}`} width={100} height={100} className='border border-stone-500/50 rounded-md h-[calc(100%/4)]'/>
-            ) : (
-                ''
-            )
+            ) : ('')
         ))
+
         ) : (
           <p
             className='uppercase text-xs text-stone-500'
@@ -40,13 +45,24 @@ export default function ImagesProduct({ images }: ImagesProductProps) {
       <div
         className='border rounded-md border-stone-500/50 flex items-center justify-center relative'
       >
-        <span
-            className='absolute cursor-pointer left-[100%] top-[calc(50% - 5px)] bg-stone-500/75 rounded-full w-[40px] h-[40px] flex justify-center items-center'
-        >
-            <Image src='/arrow.svg' width={10} height={10} alt='flecha' className='opacity-75'/>
-        </span>
+        {pathSelect != images[images.length - 1].path ? (
+            <span
+                className='absolute cursor-pointer top-[calc(50% - 5px)] bg-stone-500/75 rounded-full w-[40px] h-[40px] flex justify-center items-center'
+                style={{left: 'calc(100% - 50px)'}}
+            >
+                <Image src='/arrow.svg' width={10} height={10} alt='flecha' className='opacity-75'/>
+            </span>
+        ): ''}
 
-        <Image src={images[0].path} width={500} height={500} alt='Imagem principal' className='rounded-md min-w-full'/>
+        {pathSelect != images[0].path ? (
+            <span
+                className='absolute rotate-180 cursor-pointer left-[10px] top-[calc(50% - 5px)] bg-stone-500/75 rounded-full w-[40px] h-[40px] flex justify-center items-center'
+            >
+                <Image src='/arrow.svg' width={10} height={10} alt='flecha' className='opacity-75'/>
+            </span>
+        ): ''}
+
+        <Image src={pathSelect} width={500} height={500} alt='Imagem principal' className='rounded-md min-w-full'/>
       </div>
     </div>
   );
